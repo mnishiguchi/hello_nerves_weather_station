9e794aaa0bf9dd1e2a47a878fd54a531ca1a2a1f
Masatoshi N <nishiguchi.masa@gmail.com>
2022-02-12 10:26:40 -0500

    Configure time zone

--- sensor_hub/config/target.exs
+++ sensor_hub/config/target.exs
@@ -107,6 +107,8 @@ config :mdns_lite,
     }
   ]
 
+config :nerves_time_zones, default_time_zone: "America/New_York"
+
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 # Uncomment to use target specific configurations
--- sensor_hub/mix.exs
+++ sensor_hub/mix.exs
@@ -39,6 +39,7 @@ defmodule SensorHub.MixProject do
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.11.3", targets: @all_targets},
       {:nerves_pack, "~> 0.6.0", targets: @all_targets},
+      {:nerves_time_zones, "~> 0.1.0", targets: @all_targets},
 
       # Dependencies for specific targets
       # NOTE: It's generally low risk and recommended to follow minor version
